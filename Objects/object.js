d0d81f3b1af01b6c05139f83ef0644f24ee077b8
Mariagoretti <mariakimanigoreti@gmail.com>
2023-04-28 16:49:30 +0300

    Add

---
+++ Objects/object.js
@@ -0,0 +1,8 @@
+// Given an array of objects, each object representing a person with a name and age property,
+//write a function that returns a new array containing the names of all people who are 18 years old or older.
+const people = [
+  { name: 'Alice', age: 17 },
+  { name: 'Eunice', age: 22 },
+  { name: 'Charlie', age: 14 },
+  { name: 'Max', age: 19 },
+];
