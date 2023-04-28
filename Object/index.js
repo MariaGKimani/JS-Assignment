dff394f14d8f8a03d77836ce81d8f7ac3bb2842b
Mariagoretti <mariakimanigoreti@gmail.com>
2023-04-28 16:51:12 +0300

    Add

--- Object/index.js
+++ Object/index.js
@@ -82,7 +82,7 @@ console.log(scoringStudents)
 // Given an object representing a car, with properties for the make, model, year, and a method to display the car's information, 
 // write a function that takes the car object and adds a new method to the object called age. 
 // The age method should return the current age of the car based on the current year and the car's year property.
-const Car = {
+const car = {
   make: 'Ford',
   model: 'Ranger',
   year: 2023,
@@ -90,11 +90,13 @@ const Car = {
     console.log(`Make: ${this.make}, Model: ${this.model}, Year: ${this.year}`);
   },
 };
-function age(){
-  const currentYear = new Date().getFullYear();
-  return currentYear - this.year;
+const currentage = function(obj) {
+    obj.age = function() {
+       return fullyear - car.year
+    }
 }
-
-
-const car = new Car('Ford', 'Ranger', 2023);
-console.log(car.age()); 
+currentage(car)
+const date = new Date()
+const fullyear = date.getFullYear()
+console.log(car.age())
+console.log(car)
