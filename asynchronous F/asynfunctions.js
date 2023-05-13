9da2156ce97044f876f567f87773d9bff7041db1
Mariagoretti <mariakimanigoreti@gmail.com>
2023-05-13 09:06:33 +0300

    add classes

--- asynchronous F/asynfunctions.js
+++ asynchronous F/asynfunctions.js
@@ -1,27 +1,37 @@
-// Write an asynchronous function that accepts
-// a message string and a delay time in milliseconds.
-// The function should log the message to the console
-//  after the specified delay time.
-const asyncl = async(strF,delay)=>{
-
+// write an asynchronous function that accepts a message string and a
+// delay time in milliseconds. The function should log the message to the
+//  console after the specified delay time.
+async function logMessage(message,delay){
+    await new Promise(resolve=>setTimeout(resolve,delay));
+    console.log(message);
 }
-
-// You have an array of user IDs 
-// and a function getUserData(id) 
-// that returns a Promise with user data 
-// when given a user ID. 
-// Write an asynchronous function that 
-// fetches and logs the data for each 
+logMessage("authenticity",6000);
+// You have an array of user IDs and a function getUserData(id)
+//  that returns a Promise with user data when given a user ID. Write
+//  an asynchronous function that fetches and logs the data for each
 // user ID one by one, in sequence.
-
-const x  = getUserData(id){
-
+ async function fetchAndLogs(userIds) {
+    for (const userId of userIds) {
+      const userInfo = await(userId);
+      console.log(userInfo);
+    }
+  }
+  const userIds = [67, 405, 7168];
+async function info() {
+  try {
+    await fetchAndLogs(userIds);
+  } catch (e) {
+    console.error(e);
+  }
 }
-
-
-// You have an asynchronous function performTask() that returns a Promise.
-//  The Promise resolves if the task is successful 
-//  and rejects if there's an error.
-//   Write a function that calls performTask() 
-//   and logs a custom success message if the task is successful,
-//    and a custom error message if there's an error.
+info();
+//question3
+async function runTask() {
+    try {
+      await performTask();
+      console.log("Task completed successfully!");
+    } catch (error) {
+      console.error("An error occurred while performing the task:", error);
+    }
+  }
+  runTask();
